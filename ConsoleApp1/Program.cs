efa95143c98145d24de9076fb4b39e72b45be13f
s34540-code <sXXXX@pjwstk.edu.pl>
2026-03-17 20:46:38 +0100

    Add input validation and error message

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,3 +8,13 @@ Console.WriteLine("Change 1");
 Console.WriteLine("Enter a number:");
 string input = Console.ReadLine();
 Console.WriteLine($"You entered: {input}");
+
+
+if (!int.TryParse(input, out int number))
+{
+    Console.WriteLine("Invalid number entered.");
+}
+else
+{
+    Console.WriteLine($"Number entered: {number}");
+}
